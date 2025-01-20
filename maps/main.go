b116aac8a1b15f65436d5dcfd197b53b2073b374
Goutham Pilla <goutham2027@gmail.com>
2025-01-20 14:17:05 +0530

    golang maps

--- maps/main.go
+++ maps/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	// to define initial values
 	m1 := map[string]string{"a": "apple"}
 
+	// to create empty map
+	// make also int as second argument to define the initial capacity
 	m2 := make(map[string]string)
 	m2["a"] = "apple"
 
@@ -13,8 +16,14 @@ func main() {
 	m3 = make(map[string]string)
 	m3["a"] = "apple"
 
+	m4 := make(map[string]string, 2)
+	m4["a"] = "apple"
+	m4["b"] = "banana"
+	m4["c"] = "cherry"
+
 	fmt.Println("m1", m1)
 	fmt.Println("m2", m2)
 	fmt.Println("m3", m3)
+	fmt.Println("m3", m4)
 
 }
